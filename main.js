1c3c9dd0a3d54df6aca8f37cf079abaa9cf499d5
Vince <vincedemase@gmail.com>
2018-07-31 11:48:52 -0400

    cleaned up ui and added tooltip information

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const {app, BrowserWindow} = require('electron')
+
   
   // Keep a global reference of the window object, if you don't, the window will
   // be closed automatically when the JavaScript object is garbage collected.
@@ -47,3 +48,5 @@ const {app, BrowserWindow} = require('electron')
   
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
+
+  
